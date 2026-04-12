eba0fa71d3ae9e8080ebc0131d197d6bbb6f45be
Alvaro Separovich <alvasepro@gmail.com>
2026-04-12 16:02:15 -0300

    new Ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default defineConfig({});
+const root = fileURLToPath(new URL(".", import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(root, "index.html"),
+        uiV2: resolve(root, "ui-v2.html"),
+      },
+    },
+  },
+});
